f052ea23505f00ae219cecf99718aa45f5a63f54
Idan <idanbusso@gmail.com>
2017-01-23 14:40:04 +0200

    Add home and login pages

---
+++ src/main/java/com/bookstore/adminportal/domain/security/Role.java
@@ -0,0 +1,40 @@
+package com.bookstore.adminportal.domain.security;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Role {
+
+    @Id
+    private int roleId;
+    private String name;
+
+    @OneToMany(mappedBy = "role", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<UserRole> userRole = new HashSet<>();
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<UserRole> getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(Set<UserRole> userRole) {
+        this.userRole = userRole;
+    }
+}
---
+++ src/main/java/com/bookstore/adminportal/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.bookstore.adminportal.repository;
+
+import com.bookstore.adminportal.domain.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findByUsername(String username);
+
+    User findByEmail(String email);
+}
